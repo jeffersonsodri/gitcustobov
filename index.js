99246e46684497aed168b5f684ebed22b4b30304
jeffersonsodri <jeffersoncacula@gmail.com>
2019-03-15 21:45:50 -0400

    Mini Servidor

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const http = require('http');
 
-var a = "Jefferson Gostoso";
+const hostname = '127.0.0.1';
+const port = 3000;
 
-console.log(a);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Jefferson Gostoso  no site legal!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
