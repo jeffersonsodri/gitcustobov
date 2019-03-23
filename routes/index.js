9444c75170231f0e3f7efdba7ec06e12d20fc936
jeffersonsodri <jeffersoncacula@gmail.com>
2019-03-22 20:47:11 -0400

    Vue,js + tentativa de conexão com o banco de dados

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var mongoData = require("mongodb").MongoClient;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var db = require("../data/custobovDB")
-  res.render('index', { title: 'Jefferson gostoso' });
+  var db = connectDB();
+  // res.render('index', { title: 'Frase: Meu nome é Êmille' });
+  res.send(db.mongo.find({}).toArray());
 });
+
+
+async function connectDB(){
+
+  if(!global.database){
+      
+      const fazenda = mongoData.connect("mongodb://localhost:27017", function(err, conn){
+          if(err) return console.log("Erro de conexão");
+          
+          global.database = conn.db("custobovDB");
+          
+        });
+        return fazenda;
+      
+  }
+}
 
 module.exports = router;
