50df8a588b258eb2b0453eafe4475f219bce88a5
jeffersonsodri <jeffersoncacula@gmail.com>
2019-03-28 02:52:07 -0400

    banco sem erro e segundo committe no GITLAB 2-estrutura-do-mongodb

--- backend/routes/server.js
+++ backend/routes/server.js
@@ -1,26 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const MongoClient = require('mongodb').MongoClient;
+const mongo = require('mongodb').MongoClient;
 
 /**
  * Conecção em banco de dados
  */
 const url = 'mongodb://127.0.0.1:27017/custobov';
-MongoClient.connect(url, (err, db) => {
-  if(err) { 
-    res.send(err);
-  }
-  else {
-    console.log("Banco conectando........");
-    db.collection('fazenda').find().toArray(function(err, result){
-        if(err) res.send(err);
-        else{
-            console.log(result);
-        }
-    })      
-  }
-  
-});
+mongo.connect(url, {useNewUrlParser: true});
+
 router.get('/', function(err, res, next) {
     res.send("Meu arquivo Node")
 
